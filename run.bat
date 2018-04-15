8bd33adbc6cba57e0a4bd3cdb368a02b0ec7b32f
Lauren Hughes <lh0xfb@gmail.com>
2018-04-14 23:29:29 -0700

    stardust brush

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 
-main.exe -image=example.png -brush=brush.png -maxPrimitives=3000 -framesPerPrimitive=500 -primAlpha=0.75 -secondsPerScreenshot=180
+main.exe -image=example.png -brush=stardust.png -maxPrimitives=3000 -framesPerPrimitive=500 -primAlpha=0.75 -secondsPerScreenshot=180
